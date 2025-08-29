78cf6755ff16f5dd16f49540b3f757b82f3f9b19
bigqmope <adella.sweeta@gmail.com>
2025-08-29 15:14:52 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,25 +17,19 @@ $persegi->panjang = 10;
 $persegi->lebar = 5;
 echo "Luas: " . $persegi->hitungLuas() . "\n";         // Output: Luas: 50
 echo "Keliling: " . $persegi->hitungKeliling() . "\n"; // Output: Keliling: 30
-?>
 
-<?php
-class PersegiPanjang {
-    public $panjang;
-    public $lebar;
-    // Method untuk menghitung luas
-    public function hitungLuas() {
-        return $this->panjang * $this->lebar;
-    }
-    // Method untuk menghitung keliling
-    public function hitungKeliling(){
-        return 2 * ($this->panjang + $this->lebar);
-    }
+/*class Produk {
+    public $nama;
+    public $harga;
+    public $stok;
+
+    //method menampilkan stok
+    public function tampilkanInfo()
 }
-// Contoh penggunaan:
-$persegi = new PersegiPanjang();
-$persegi->panjang = 10;
-$persegi->lebar = 5;
-echo "Luas: " . $persegi->hitungLuas() . "\n";         // Output: Luas: 50
-echo "Keliling: " . $persegi->hitungKeliling() . "\n"; // Output: Keliling: 30
+// membuat objek:
+$belanja = new $Produk();
+$belanja->nama = "Alpukat";
+$belanja->harga = 20000;
+$belanja->stok = "ada";*/
+
 ?>
