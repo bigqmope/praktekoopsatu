1a932a70d82b29695e100db0d667e38ce295e9ff
bigqmope <adella.sweeta@gmail.com>
2025-08-29 15:31:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-class PersegiPanjang {
+/*class PersegiPanjang {
     public $panjang;
     public $lebar;
     // Method untuk menghitung luas
@@ -16,20 +16,38 @@ $persegi = new PersegiPanjang();
 $persegi->panjang = 10;
 $persegi->lebar = 5;
 echo "Luas: " . $persegi->hitungLuas() . "\n";         // Output: Luas: 50
-echo "Keliling: " . $persegi->hitungKeliling() . "\n"; // Output: Keliling: 30
+echo "Keliling: " . $persegi->hitungKeliling() . "\n"; // Output: Keliling: 30*/
 
-/*class Produk {
+class Produk {
     public $nama;
     public $harga;
     public $stok;
 
     //method menampilkan stok
-    public function tampilkanInfo()
+    public function tampilkanInfo() {
+        return "Buah $this->nama dengan harga $this->harga, stok sisa $this->stok";
+    }
+    //method beliProduk($jumlah)
+    public function beliProduk($jumlah) {
+        if ($jumlah <= $this->stok {
+            $this->stok -= $jumlah;
+            return "Pembelian berhasil. Sisa stok: $this->stok";
+        } else {
+            return "Stok tidak cukup untuk membeli $jumlah buah.";
+        }
+    }
 }
 // membuat objek:
 $belanja = new $Produk();
 $belanja->nama = "Alpukat";
 $belanja->harga = 20000;
-$belanja->stok = "ada";*/
+$belanja->stok = 30;
 
+//menampilkan output
+echo $belanja->tampilkanInfo();
+echo "/n"
+echo $belanja->beliProduk(2);
+echo "/n"
+echo $belanja->tampilkanInfo();
+echo "/n"
 ?>
