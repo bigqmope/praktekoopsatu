522197f8ffecd26d109f446b0c12231f17cf03d3
bigqmope <adella.sweeta@gmail.com>
2025-09-11 01:00:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ $tugas = [
     <title>Menu Tugas PBO</title>
     <style>
         body { font-family: Arial, sans-serif; margin: 40px; background: #e3dade; }
-        h1 { color: #333; align-text: center;}
+        h1 { color: #333; text-align: center;}
         ul { list-style: none; padding: 0; }
         li { margin: 10px 0; }
-        a { text-decoration: none; color: white; background: #d8959b; padding: 10px 15px; border-radius: 5px; align-text: center;}
-        a:hover { background: #0056b3; }
+        a { text-decoration: none; color: white; background: #d8959b; padding: 10px 15px; border-radius: 5px; text-align: center;}
+        a:hover { background: #d8959d; }
     </style>
 </head>
 <body>
