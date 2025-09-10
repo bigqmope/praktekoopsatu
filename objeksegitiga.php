888bc4a772034d2926e4cfc511323e6a65b278ac
bigqmope <adella.sweeta@gmail.com>
2025-09-11 00:31:13 +0700

    Create objeksegitiga.php

---
+++ objeksegitiga.php
@@ -0,0 +1,9 @@
+<?php
+require_once "classSegitiga.php";
+
+$segitiga1 = new Segitiga(6, 8, 6, 10, 8);
+$segitiga1->CekInfo();
+
+$segitiga2 = new Segitiga(5, 5, 5, 5, 4.33);
+$segitiga2->CekInfo();
+?>
