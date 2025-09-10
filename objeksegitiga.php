41968355ce2f33dc96736f4f8426dbc0e8078033
bigqmope <adella.sweeta@gmail.com>
2025-09-11 00:32:37 +0700

    Update objeksegitiga.php

--- objeksegitiga.php
+++ objeksegitiga.php
@@ -2,8 +2,8 @@
 require_once "classSegitiga.php";
 
 $segitiga1 = new Segitiga(6, 8, 6, 10, 8);
-$segitiga1->CekInfo();
+$segitiga1->CetakInfo();
 
 $segitiga2 = new Segitiga(5, 5, 5, 5, 4.33);
-$segitiga2->CekInfo();
+$segitiga2->CetakInfo();
 ?>
