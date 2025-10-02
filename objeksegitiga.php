bda9386cd85fa3bbd40bc31321a027e58726a849
bigqmope <adella.sweeta@gmail.com>
2025-10-02 17:41:36 +0700

    Delete objeksegitiga.php

--- objeksegitiga.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once "classSegitiga.php";
-
-$segitiga1 = new Segitiga(6, 8, 6, 10, 8);
-$segitiga1->CetakInfo();
-
-$segitiga2 = new Segitiga(5, 5, 5, 5, 4.33);
-$segitiga2->CetakInfo();
-?>
